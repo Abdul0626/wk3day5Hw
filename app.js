2251c6da4449b13365536e561ef2ddbdc4890dbe
MacUer <macuer@MacUers-MacBook-Pro.local>
2022-04-17 18:03:36 -0400

    some message

---
+++ app.js
@@ -0,0 +1,64 @@
+// Addition
+var a 50;
+
+var b 10;
+
+function addSum(param1,param2){
+    console.log(param1 + param2);
+}
+
+addSum(a,b);
+
+const addSum = (param1, param2) => {
+    console.log(param1 + param2);
+}
+addSum(a,b);
+
+// Subtraction
+var a 50;
+
+var b 10;
+
+function subtractSum(param1,param2){
+    console.log(param1 - param2);
+}
+
+subtractSum(a,b);
+
+const subtractSum = (param1, param2) => {
+    console.log(param1 - param2);
+}
+subtractSum(a,b);
+
+// Multiplication
+var a 50;
+
+var b 10;
+
+function multiplySum(param1, param2){
+    console.log(param1 * param2);
+}
+
+multiplySum(a,b);
+
+const multiplySum = (param1, param2) => {
+    console.log(param1 * param2);
+}
+multiplySum(a,b);
+
+// Division
+
+var a 50;
+
+var b 10;
+
+function divideSum(param1, param2){
+    console.log(param1 / param2);
+}
+
+divideSum(a,b);
+
+const divideSum = (param1, param2) => {
+    console.log(param1 / param2);
+}
+divideSum(a,b);
